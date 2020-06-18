8fcc78d73660f9d4a42725a80c084f43f519f9e0
ugogo <hello@onali-ugo.fr>
2020-06-18 19:28:05 +0200

    update prettier and eslint config

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-import { ExampleComponent } from 'react-verification-code'
-import 'react-verification-code/dist/index.css'
+import { ExampleComponent } from 'react-verification-code';
+import 'react-verification-code/dist/index.css';
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+  return <ExampleComponent text='Create React Library Example 😄' />;
+};
 
-export default App
+export default App;
