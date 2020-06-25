125a7c8ea6bd2b18ea36a4acdf5a9dec0a4942ce
ugogo <hello@onali-ugo.fr>
2020-06-25 16:21:45 +0200

    update broken examples

--- examples/basic/src/index.tsx
+++ examples/basic/src/index.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReactInputVerificationCode from 'react-input-verification-code';
 
-import 'react-input-verification-code/dist/index.css';
 import './index.css';
 
 ReactDOM.render(
--- examples/custom-styles/src/index.tsx
+++ examples/custom-styles/src/index.tsx
@@ -2,18 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ReactInputVerificationCode from 'react-input-verification-code';
 
-import 'react-input-verification-code/dist/index.css';
 import './index.css';
 
 ReactDOM.render(
-  <main>
-    <h1>Authenticate your account</h1>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-      gravida ligula nulla, eu molestie dui vehicula non. Sed volutpat mollis
-      sapien sit amet eleifend. Sed vitae molestie lectus
-    </p>
+  <div className='custom-styles'>
     <ReactInputVerificationCode placeholder='' onChange={console.log} />
-  </main>,
+  </div>,
   document.getElementById('root')
 );
