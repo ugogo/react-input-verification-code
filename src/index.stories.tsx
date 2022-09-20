58fa11ea95d51604cdee2b83039c3016e7ab10d9
ugogo <ugo.j.onali@gmail.com>
2022-09-20 21:02:25 +0200

    feat: add custom-styles story

--- src/index.stories.tsx
+++ src/index.stories.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import ReactInputVerificationCode from './';
+import styled from 'styled-components';
 
 export default {
   title: 'ReactInputVerificationCode',
@@ -20,6 +21,11 @@ const Template: ComponentStory<typeof ReactInputVerificationCode> = (args) => (
 );
 
 export const Default = Template.bind({});
+
+export const WithDefaultValue = Template.bind({});
+WithDefaultValue.args = {
+  value: '7890',
+};
 
 export const CustomLength = Template.bind({});
 CustomLength.args = {
@@ -44,7 +50,31 @@ CustomType.argTypes = {
   },
 };
 
-export const WithDefaultValue = Template.bind({});
-WithDefaultValue.args = {
-  value: '7890',
-};
+const CustomStylesContainer = styled.div`
+  position: relative;
+
+  .ReactInputVerificationCode-item {
+    width: 2.5rem;
+    height: 3.5rem;
+    color: #262626;
+    font-weight: 500;
+    border-radius: 0;
+    border-bottom: 1px solid #ddd;
+    transition: border-bottom-color 0.2s ease-out;
+  }
+
+  .ReactInputVerificationCode-item:focus {
+    border-bottom-color: #046cde;
+  }
+
+  .ReactInputVerificationCode-item,
+  .ReactInputVerificationCode-item:focus {
+    box-shadow: none;
+  }
+`;
+
+export const CustomStyles = () => (
+  <CustomStylesContainer>
+    <ReactInputVerificationCode placeholder='' />
+  </CustomStylesContainer>
+);
